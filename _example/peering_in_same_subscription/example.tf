44b24cd4ec5dfee812c9dcc1844f7d012bdf6cbf
chahalvikas2022 <chahalvikash2022@gmail.com>
2026-06-16 18:39:10 +0530

    inital commit

---
+++ _example/peering_in_same_subscription/example.tf
@@ -0,0 +1,57 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  name        = "opsstation"
+  environment = "test"
+}
+
+module "resource_group_1" {
+  source      = "git::https://github.com/chahalvikas2022/terraform-azure-resource-group.git"
+  name        = local.name
+  environment = local.environment
+  location    = "West Europe"
+}
+
+
+module "resource_group_2" {
+  source      = "git::https://github.com/chahalvikas2022/terraform-azure-resource-group.git"
+  name        = "test"
+  environment = local.environment
+  location    = "East US"
+}
+
+
+
+#Vnet
+module "vnet" {
+  source              = "git::https://github.com/chahalvikas2022/terraform-azure-vnet.git"
+  name                = local.name
+  environment         = local.environment
+  resource_group_name = module.resource_group_1.resource_group_name
+  location            = module.resource_group_1.resource_group_location
+  address_spaces      = ["10.0.0.0/16"]
+}
+
+#Vnet
+module "vnet_remote" {
+  source              = "git::https://github.com/chahalvikas2022/terraform-azure-vnet.git"
+  name                = local.name
+  environment         = local.environment
+  resource_group_name = module.resource_group_2.resource_group_name
+  location            = module.resource_group_2.resource_group_location
+  address_spaces      = ["20.0.0.0/16"]
+}
+
+module "vnet_peering" {
+  source                = "../.."
+  enabled_peering       = true
+  resource_group_1_name = module.resource_group_1.resource_group_name
+  resource_group_2_name = module.resource_group_2.resource_group_name
+  different_rg          = true
+  vnet_1_name           = module.vnet.vnet_name[0]
+  vnet_1_id             = module.vnet.vnet_id[0]
+  vnet_2_name           = module.vnet_remote.vnet_name[0]
+  vnet_2_id             = module.vnet_remote.vnet_id[0]
+}
